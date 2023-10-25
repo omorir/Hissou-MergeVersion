c6f0533e6ab31a4d8af439d2450d0089cf9da7ea
omorir <71996772+omorir@users.noreply.github.com>
2023-10-26 00:47:01 +0900

    assets5
    Enemy

---
+++ Assets/Enemy/EnemyBird/BirdMotionscript.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdMotionscript : MonoBehaviour
+{
+    //public static Animator animator;
+    public Animator animator;
+
+    //public static bool birdflag;
+    //private bool flagPRI;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (birdflag == true)
+        //{
+        //    flagPRI = true;
+        //}
+        //else
+        //{
+        //    flagPRI = false;
+        //}
+
+       
+    }
+}
